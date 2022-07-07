454793bae772cb9e331db020c7757ab346871dc0
Erik <erichou@rutgers.edu>
2022-07-06 20:19:04 -0400

    generate project

---
+++ src/main/java/com/example/springbootoauthlab/SpringBootOauthLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootoauthlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauthLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauthLabApplication.class, args);
+	}
+
+}
